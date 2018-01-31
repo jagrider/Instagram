d2dabd4cc9a8f82cd41fa01b4178e8a7b819d58c
Jonathan Grider <jagrider@purdue.edu>
2018-01-31 14:42:38 -0500

    Initial commit

--- Instagram/AppDelegate.swift
+++ Instagram/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Parse
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,7 +16,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-    // Override point for customization after application launch.
+    
+    // Link Parse to heroku server
+    Parse.initialize(
+      with: ParseClientConfiguration(block: { (configuration: ParseMutableClientConfiguration) -> Void in
+        configuration.applicationId = "Instagram"
+        configuration.clientKey = "eq78nfh8hiehwihqfniy"  // set to nil assuming you have not set clientKey
+        configuration.server = "https://fathomless-dawn-95069.herokuapp.com/parse"
+      })
+    )
+    
+    
     return true
   }
 
---
+++ Instagram/LoginViewController.swift
@@ -0,0 +1,50 @@
+//
+//  LoginViewController.swift
+//  Instagram
+//
+//  Created by Jonathan Grider on 1/31/18.
+//  Copyright © 2018 Jonathan Grider. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class LoginViewController: UIViewController {
+  
+  @IBOutlet weak var usernameField: UITextField!
+  @IBOutlet weak var passwordField: UITextField!
+  
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    // Do any additional setup after loading the view.
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+  
+  @IBAction func onSignIn(_ sender: Any) {
+    
+    
+  }
+  
+  @IBAction func onSignUp(_ sender: Any) {
+    
+    
+  }
+  
+  
+  /*
+   // MARK: - Navigation
+   
+   // In a storyboard-based application, you will often want to do a little preparation before navigation
+   override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+   // Get the new view controller using segue.destinationViewController.
+   // Pass the selected object to the new view controller.
+   }
+   */
+  
+}
